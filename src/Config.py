b1996ab6f5e50f32ca4a7a1d57980a6c0f5e7df7
Firstman <azazo1@qq.com>
2021-12-05 14:31:14 +0800

    1.修改了fileTransport命令的规则 2.创建了fileTransport-get的test

--- src/Config.py
+++ src/Config.py
@@ -12,7 +12,7 @@ class Config:
     user = ('', '')  # 图片发送邮箱 SMTP 账号密码
     password = 'MyComputerAzazo1'  # 锁屏默认密码
     readRange = 32768  # 套接字一次读取长度（字节）
-    longestCommand = 2 ** 15  # 最长命令长度（字节）
+    longestCommand = 32768  # 最长命令长度（字节）
     socketTimeoutSec = 5  # 套接字超时时间（秒）
     longestShowTextTime = 10000  # showText命令最长显示时间（毫秒）
     authenticationTimeoutMilli = 10000  # 鉴权过期时间（毫秒）
@@ -22,7 +22,7 @@ class Config:
     key = "azazo1Bestbdsrjpgaihbaneprjaerg".encode(encoding)  # 传输加密密钥
     clearDeadClient = True  # 服务器是否定期删除断开连接的客户端
     processQueueMaxsize = 100  # 多进程时 Queue 最大尺寸
-    fileTransportMaxSize = 8192  # 传输文件内容最大大小（字节）
+    fileTransportMaxSize = 8192  # 传输文件内容(原)最大大小（字节）
     variablesFile = 'vars.json'
     initialVars = {
         "blockTaskmgr": False,
